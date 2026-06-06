0a804e6df845a09b1ec257ee4504d8c3cf904769
venu <venudhannoju@gmail.com>
2026-06-06 11:23:55 +0530

    05-06-26

---
+++ 05-06-26/responsive-student-management/Mainly.java
@@ -0,0 +1,12 @@
+public class Mainly{
+    public static void main(String args[]){
+        String s="vennu";
+        int i=3;
+        float f=5.2f;
+        boolean b=true;
+        char c='a';
+        System.out.println(s + i +f +b+ c);
+        // System.out.println("heyyy "+name);
+        // System.out.println(num);
+    }
+}
