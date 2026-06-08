191ac4be62111c8474fbe9acce91695c1094340d
Venu Dhannoju <venudhannoju@gmail.com>
2026-06-08 06:54:09 +0000

    college event management

---
+++ 08-06-26/college-event-management/js/script.js
@@ -0,0 +1,26 @@
+function toggleDarkMode(){
+    document.body.classList.toggle("dark-mode");
+}
+
+(function () {
+'use strict';
+
+var forms = document.querySelectorAll('.needs-validation');
+
+Array.prototype.slice.call(forms)
+.forEach(function(form){
+
+form.addEventListener('submit',function(event){
+
+if(!form.checkValidity()){
+event.preventDefault();
+event.stopPropagation();
+}
+
+form.classList.add('was-validated');
+
+},false);
+
+});
+
+})();
